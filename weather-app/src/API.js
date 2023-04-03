93e5a8b2620b2b6962a07202963295d5231c8c72
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2023-04-03 21:28:07 +0300

    initial commit + basic structure

---
+++ weather-app/src/API.js
@@ -0,0 +1,9 @@
+export const GeoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "48524f7c61msh6617bcb98fdec60p1d43a0jsnad76e2a01148",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
