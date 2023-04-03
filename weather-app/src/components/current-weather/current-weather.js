12b85ec897750d28d12fcb34beaa0d4b15b26349
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2023-04-03 22:20:07 +0300

    current weather + forecast init

--- weather-app/src/components/current-weather/current-weather.js
+++ weather-app/src/components/current-weather/current-weather.js
@@ -1,13 +1,45 @@
 import "./current-weather.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Bucharest</p>
-        <p className="weather-description">Sunny</p>
+        <div>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
+        </div>
+        <img
+          src={`icons/${data.weather[0].icon}`}
+          alt="weather"
+          className="weather-icon"
+        />
       </div>
-      <img src="" alt="weather" className="weather-icon" />
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-label">
+              {Math.round(data.main.feels_like)}°C
+            </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-label">{data.wind.speed}km/h</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-label">{data.main.humidity}%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-label">{data.main.pressure}hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
